7439bca235044bb0c62c6a2ceb1add07ab17dc27
Cbiltps <1239669254@qq.com>
2021-07-24 15:29:23 +0800

    上课刷题

---
+++ 出现多少个9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int i = 0;
+	int count = 0;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 10 == 9)
+			count++;
+		if (i / 10 == 9)
+			count++;
+	}
+	printf("共有%d个数\n", count);
+	return 0;
+}
