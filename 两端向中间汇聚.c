2f460eeeecca9591a2b8b9c8e3765648e1cb83dc
Cbiltps <1239669254@qq.com>
2021-07-24 18:10:38 +0800

    上课刷题

---
+++ 两端向中间汇聚.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+
+int main()
+{
+	char arr1[] = "welcome to bit!!!!! ";
+	char arr2[] = "                    ";
+
+	int left = 0;
+	int right = strlen(arr2) - 1;
+
+	while (left <= right)
+	{
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		printf("%s\n", arr2);
+		Sleep(1000);
+		system("cls");//执行系统命令—清空屏幕
+		left++;
+		right--;
+	}
+	printf("%s\n", arr2);
+	return 0;
+}
